c09cd62cd6f1966744be3e88571db763ca5ac610
ken.mae <kenmae86@gmail.com>
2020-05-27 15:33:01 +0000

    status

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,6 +1,6 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">タスクリスト</a>
+        <a class="navbar-brand" href="/">一覧</a>
          
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
