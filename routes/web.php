a81e0858aec093b1d9354e8fc5b0b0cb2c78bc75
ken.mae <kenmae86@gmail.com>
2020-05-30 09:31:49 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,15 @@
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
 
+// ログイン認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
